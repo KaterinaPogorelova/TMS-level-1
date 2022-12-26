155fd3a2232de0e3710a618dd9195ba1c5356c1c
Katerina Pogorelova <katrinapogorelova98@gmail.com>
2022-12-26 13:40:04 +0100

    added new sections

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	loop: true,
+	initialSlide: 1,
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
